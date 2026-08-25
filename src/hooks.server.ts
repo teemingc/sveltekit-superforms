77848600a74b4843a47b45c762dfcc87d01af128
Andreas Söderlund <ciscoheat@gmail.com>
2026-08-25 23:44:24 +0200

    Deprecation fix in error call

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,8 @@
-import { error, type Handle } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
+export const handle = async ({ event, resolve }) => {
 	if (event.request.method === 'POST' && event.request.url.includes('?throw-hooks-error')) {
-		error(403, {
-			message: 'Hooks error'
-		});
+		error(403, 'Hooks error');
 	}
 
 	return await resolve(event);
